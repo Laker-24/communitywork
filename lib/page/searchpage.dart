ddff475470f022c0612cf2ef1d9f6146b9b0c12f
Laker-24 <2722039868@qq.com>
2023-08-11 16:16:06 +0800

    最新

--- lib/page/searchpage.dart
+++ lib/page/searchpage.dart
@@ -9,16 +9,27 @@ class Searchpage extends StatefulWidget {
 class _SearchpageState extends State<Searchpage> {
   @override
   Widget build(BuildContext context) {
+    Widget divider1 = Divider(
+      color: Colors.blue,
+    );
+    Widget divider2 = Divider(color: Colors.green);
     return Scaffold(
       body: Center(
-        child:Column(
-          children: <Widget>[
-            Container(
-              
-            )
-          ],
-        )
-      ),
+          child: Column(
+        children: <Widget>[
+          ListView.separated(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: 100,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(title: Text("$index"));
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return index % 2 == 0 ? divider1 : divider2;
+            },
+          )
+        ],
+      )),
     );
   }
 }
